3563c2afe166f2efccd5e8c2b3ce44d4258924a8
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-08-17 15:58:14 +0300

    DONE

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const employee = {
+// declare the variable 
+const employee={
     name : "Sam",
-    streetAddress :  "11 Broadway",
-        
+    streetAddress : '11 Broadway'
 }
+// function   updateEmployeeWithKeyAndValue(employee, key, value)
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
- const newEmployee ={...employee}
- newEmployee[key] = value
- return (newEmployee)
-}
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-employee[key] = value
-    return (employee)
-   }
-
-   function deleteFromEmployeeByKey(employee, key){
+function   updateEmployeeWithKeyAndValue(employee, key, value){
     const newEmployee ={...employee}
+    newEmployee[key]  = value
+    return newEmployee
+}
+// function   destructivelyupdateEmployeeWithKeyAndValue(employee, key, value)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value
+    return employee
+}
+// function deleteFromEmployeeByKey(employee, key)
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee= {...employee}
     delete newEmployee[key]
-    return (newEmployee)
-   }
-   
-   function destructivelyDeleteFromEmployeeByKey(employee, key){
+    return newEmployee
+}
+// deleteFromEmployeeByKey(employee, key)
+function destructivelyDeleteFromEmployeeByKey(employee, key){
     delete employee[key]
-    return (employee)
-   }
+    return employee
+}
